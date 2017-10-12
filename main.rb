c7cd7450dca557ed6985694e1c021a0eab773699
ryanmonro <ryan@ryanmonro.info>
2017-10-12 12:25:53 +1100

    Bike data fetching ok

--- main.rb
+++ main.rb
@@ -5,6 +5,17 @@ require 'soda/client'
 require 'sinatra/reloader'
 
 get '/' do
+
+
+  client = SODA::Client.new({:domain => "data.melbourne.vic.gov.au"})
+
+  @results = client.get("qnjw-wgaj", :$limit => 5000)
+
+  # puts "Got #{results.count} results. Dumping first results:"
+  # results.first.each do |k, v|
+  #   puts "#{k}: #{v}"
+  # end
+
   erb :index
 end
 
